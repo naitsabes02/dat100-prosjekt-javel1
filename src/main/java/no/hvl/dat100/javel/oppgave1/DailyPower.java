db4514fca544794129bba200ae0e241fc99b5b23
Little <rekere12@gmail.com>
2025-10-14 13:12:39 +0200

    Oppdatert oppgave 1
    oppdatert Oppgave 1 - DailyPower/DayMain

--- src/main/java/no/hvl/dat100/javel/oppgave1/DailyPower.java
+++ src/main/java/no/hvl/dat100/javel/oppgave1/DailyPower.java
@@ -6,6 +6,11 @@ public class DailyPower {
     public static void printPowerPrices(double[] prices) {
 
         // TODO
+        for (int i = 0; i < prices.length; i++) {
+            System.out.printf("%.2f kWh ", prices[i]);
+        }
+        System.out.println();
+
 
     }
 
@@ -13,8 +18,12 @@ public class DailyPower {
     public static void printPowerUsage(double[] usage) {
 
         // TODO
-
+        for (int i = 0; i < usage.length; i++) {
+            System.out.printf("%.2f NOK ", usage[i]);
+        }
+        System.out.println();
     }
+
 
     // c) compute power usage for a single day
     public static double computePowerUsage(double[] usage) {
@@ -22,6 +31,10 @@ public class DailyPower {
         double sum = 0;
 
         // TODO
+        for (int stab = 0; stab < usage.length; stab++) {
+            sum = sum + usage[stab];
+        }
+
 
         return sum;
     }
@@ -31,11 +44,17 @@ public class DailyPower {
 
         double price = 0;
 
-        // TODO
+
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = 0; j < usage.length; j++) {
+                price = prices[i] * usage[j];
+
+            }
+            System.out.printf("%.2f Straumforbruk pr time: ", price);
+        }
 
         return price;
     }
-
     // e) compute power support for a given usage and price
     private static final double THRESHOLD = 0.9375;
     private static final double PERCENTAGE = 0.9;
@@ -44,48 +63,75 @@ public class DailyPower {
 
         double support = 0;
 
+
         // TODO
+        if (price >= THRESHOLD) {
+            support = PERCENTAGE * (price - THRESHOLD) * usage;
+        } else {
+            support = 0;
+        }
 
         return support;
     }
+
 
     // f) compute power support for a single day
+
     public static double computePowerSupport(double[] usage, double[] prices) {
-
-        double support = 0;
-
-        // TODO
-
-        return support;
+        double totalSupport = 0;
+        for (int i = 0; i < usage.length; i++) {
+            double support = getSupport(usage[i], prices[i]);
+            totalSupport += support;
+        }
+        System.out.println("Total støtte: " + totalSupport);
+        return totalSupport;
     }
+
 
     private static final double NORGESPRIS_KWH = 0.5;
 
-    // g) compute norges pris for a single day
+    // h) compute norges pris for a single day
     public static double computeNorgesPrice(double[] usage) {
 
         double price = 0;
+        double NP = 0.50;
 
-        // TODO
+        double sum = 0;
+        for (int stab = 0; stab < usage.length; stab++) {
+            sum = sum + usage[stab];
+        }
 
+        price = sum * NP;
         return price;
     }
+
 
     // g) compute peak usage during a single day
     public static double findPeakUsage(double[] usage) {
 
+        double totalusage = 0;
         double temp_max = 0;
 
-        // TODO
+        for (int i = 0; i < usage.length; i++) {
+            totalusage += usage[i];
+        }
+        temp_max = totalusage * 0.5;
+
 
         return temp_max;
+
     }
 
+    // i)
     public static double findAvgPower(double[] usage) {
 
         double average = 0;
 
-        // TODO
+        double sum = 0;
+        for(int stab = 0; stab < usage.length; stab++) {
+            sum = sum + usage[stab];
+        }
+        average = sum / usage.length;
 
         return average;
     }
--- src/main/java/no/hvl/dat100/javel/oppgave1/DayMain.java
+++ src/main/java/no/hvl/dat100/javel/oppgave1/DayMain.java
@@ -11,20 +11,50 @@ public class DayMain {
 
         double[] powerprices_day = DayPowerData.powerprices_day;
 
+
         System.out.println("==============");
         System.out.println("OPPGAVE 1");
         System.out.println("==============");
         System.out.println();
 
-        System.out.println();
 
         /*
         TODO
-
-         Write code that tests the methods you implement in the DailyPower class
-         Remember to teste the methods as you implement them
-         Remember to also to check that you get the expected results
          */
+        // Del A
+        System.out.println("Strømpriser for døgnet: ");
+        DailyPower.printPowerPrices(DayPowerData.powerprices_day);
+
+        // Del B
+        System.out.println("Straumforbruk: ");
+        DailyPower.printPowerUsage(DayPowerData.powerusage_day);
+
+        // Del C
+        double sum = DailyPower.computePowerUsage(powerusage_day);
+
+        System.out.println("Strøm for en dag: " + sum);
+
+        // Del D
+        System.out.println("Strømforbruk: ");
+        DailyPower.computeSpotPrice(DayPowerData.powerprices_day,powerusage_day);
+
+
+        // Del F
+        double støtte = DailyPower.computePowerSupport(DayPowerData.powerusage_day,DayPowerData.powerprices_day);
+        System.out.printf("Total strømstøtte for dagen: %.2f NOK%n", støtte);
+
+        // Del G
+        double norgespris = DailyPower.computeNorgesPrice(powerusage_day);
+        System.out.printf("Total Norgespris for dagen: %.2f NOK%n", norgespris);
+
+        // Del H
+
+        double maksBruk = DailyPower.findPeakUsage(DayPowerData.powerusage_day);
+        System.out.printf("Når brukes strømmens mest på dagen: %.2f NOK%n", maksBruk);
+        // Del I
+        double averagePower = DailyPower.findAvgPower(DayPowerData.powerusage_day);
+        System.out.printf("Gjennomsnittlig strømbruk for dagen: %.2f NOK%n", averagePower);
+
 
     }
 }
