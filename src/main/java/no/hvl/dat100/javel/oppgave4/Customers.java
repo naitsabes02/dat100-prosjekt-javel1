bc34e2813f2d466c4ebab67ce89bd8fea075d4ec
Little <rekere12@gmail.com>
2025-10-21 14:43:29 +0200

    4e
    ja

--- src/main/java/no/hvl/dat100/javel/oppgave4/Customers.java
+++ src/main/java/no/hvl/dat100/javel/oppgave4/Customers.java
@@ -68,7 +68,16 @@ public class Customers {
 
         // TODO
 
-        return c;
+        for (int i = 0; i < customers.length; i++) {
+            if (customers[i] != null && customers[i].getCustomer_id() == customer_id) {
+                Customer removed = customers[i];
+                customers[i] = null;
+                return removed;
+            }
+
+        }
+
+        return null;
     }
 
     // f) return reference table with all customers
